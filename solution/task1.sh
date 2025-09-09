0da09abfd7793cc0949c20a37307b39f0093d76e
Irina Mamzurina <mamzurina.ira@yandex.ru>
2025-09-09 23:41:29 +0700

    solution updated

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd ~/mpt-command-line-basics-pogromist300k/solution
 temp_dir=$(mktemp -d tmpd.XXXXXX)
 cd $temp_dir
-tar -xf ~/mpt-command-line-basics-pogromist300k/data/archive-part1.tar
-unzip ~/mpt-command-line-basics-pogromist300k/data/archive-part2.zip
+tar -xf ../../data/archive-part1.tar
+unzip ../../data/archive-part2.zip
 tar -czf archive-combined.tar.gz *
-mv archive-combined.tar.gz ~/mpt-command-line-basics-pogromist300k/data
+mv archive-combined.tar.gz ../../data
