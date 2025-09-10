a5e766d981268a9bda7d9e28db95cd69f4f43973
Irina Mamzurina <mamzurina.ira@yandex.ru>
2025-09-10 13:48:03 +0700

    solution updated

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-foo_path=$(find ../data -name foo.txt)
-bar_path=$(find ../data -name bar.txt)
-baz_path=$(find ../data -name baz.txt)
+foo_path=$(find -name foo.txt)
+bar_path=$(find -name bar.txt)
+baz_path=$(find -name baz.txt)
 cat $foo_path $bar_path $baz_path
